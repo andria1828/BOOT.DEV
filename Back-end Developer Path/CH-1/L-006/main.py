5148e27707a49e36e77ab51667847299c2adbf70
andria1828 <andriakobakhidze615@gmail.com>
2025-10-22 17:36:48 +0400

    commit

---
+++ Back-end Developer Path/CH-1/L-006/main.py
@@ -0,0 +1,31 @@
+"""
+What Is “Code”?
+Code is just a series of instructions for a computer to follow one after another. Programs can have a lot of instructions.
+
+The Boot.dev backend has 46,119 lines of code as I write this... and it's much smaller than some other projects I've worked on.
+
+Remember how we used the print() instruction to print text to the console? We can also use it to print numbers. This prints the number 42:
+
+print(42)
+
+Well, addition is one of the most common instructions in programming. This also prints the number 42:
+
+print(40 + 2)
+
+It first calculates the sum inside the parentheses, and then prints the result.
+
+Assignment
+Simple addition is used all the time in game development. In Fantasy Quest, we want weapons to deal bonus damage when they're enchanted.
+
+Our hero's sword deals 250 damage normally, but should deal an additional 75 damage when it's enchanted.
+
+Calculate and print the result of 250 + 75
+
+
+
+
+
+
+"""
+
+print(250 + 75)
---
+++ Back-end Developer Path/CH-1/L-012/main.py
@@ -0,0 +1,15 @@
+"""
+Game Statistics
+The marketing team is asking for some stats about our Fantasy Quest game servers. They'd like to know the average number of players across our 4 servers.
+
+Assignment
+Debug the code so that it correctly prints the solution to the math problem.
+
+The syntax is wrong. Why is it treating the numbers like words?
+
+Tip
+Strings have quotations but integers don't!
+
+"""
+
+print((250 + 241 + 244 + 255) / 4)
